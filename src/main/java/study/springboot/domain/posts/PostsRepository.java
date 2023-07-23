8d3bb0eede04470dd947e281f88de2a4828bf4fb
codingsohyun <sshyuneee@gmail.com>
2023-07-23 14:28:10 +0900

    7/23 공부

--- src/main/java/study/springboot/domain/posts/PostsRepository.java
+++ src/main/java/study/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,7 @@
 package study.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import study.springboot.domain.posts.Posts;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
